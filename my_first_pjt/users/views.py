9e6267c0bb09d64b12e2eff61008e05d1a700205
“personal” <ouxrlo3@gmail.com>
2025-01-13 16:57:58 +0900

    set_users

--- my_first_pjt/users/views.py
+++ my_first_pjt/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from  django.http import HttpResponse
 
 def index(request):
-    response = HttpResponse('<h1> Home </h1>')
-    return response
+    return render(request, 'index.html')
+
+def users(request):
+    return render(request, 'users.html')
