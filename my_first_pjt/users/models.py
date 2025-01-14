1d394d52b5eec04df831a82e3aa95a72ff50b6b1
“personal” <ouxrlo3@gmail.com>
2025-01-14 22:08:47 +0900

    circular_import

--- my_first_pjt/users/models.py
+++ my_first_pjt/users/models.py
@@ -1,28 +1,21 @@
-### 사용자 인증 시스템을 구성할 때 사용되는 모델
-from django.contrib.auth.models import AbstractUser, User
 from django.db import models
-
-class User(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True, null=True)
-
-    
-    def __str__(self):
-        return self.title 
+from django.contrib.auth.models import AbstractUser  
 
 class CustomUser(AbstractUser):
-
     birthday = models.DateField(null=True, blank=True)
-    profile_image = models.ImageField(upload_to='profile_pics/', null=True, blank=True) 
-    # 소개글 
+    profile_image = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.username
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('users.CustomUser', on_delete=models.CASCADE)  # 문자열로 참조
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
 
 
 
